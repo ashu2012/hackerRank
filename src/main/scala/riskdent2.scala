34c9a58787ad2a29fcecc7a527a67b93a70c0d55
ashutosh pathak <ashutosh.pathak@Honeywell.com>
2018-05-14 01:00:06 +0530

    two priority queue

--- src/main/scala/riskdent2.scala
+++ src/main/scala/riskdent2.scala
@@ -14,8 +14,6 @@ Not all test cases passed
 
 import java.io.{PrintStream, PrintWriter}
 
-import Solution.sortedList
-
 import scala.collection.mutable
 import scala.io.StdIn
 import scala.language.implicitConversions
@@ -30,11 +28,18 @@ object Solution {
    */
   case class order(startTime : Int, processingTime:Int)
 
-  var sortedList = new scala.collection.mutable.ArrayBuffer[order]()
+  var customerList = new scala.collection.mutable.PriorityQueue[order]().reverse
 
   implicit def orderCompare(o:order):Ordered[order]=new Ordered[order]{
     def compare(other: order):Int =( o.startTime.compare(other.startTime) ) match {
       case 0 => o.processingTime.compareTo(other.processingTime)
+      case c => c
+    }
+  }
+
+  implicit  val newOrdering =  new Ordered[order]{
+    def compare(other: order):Int =( o.startTime.compare(other.processingTime) ) match {
+      case 0 => o.processingTime.compareTo(other.startTime)
       case c => c
     }
   }
@@ -50,33 +55,37 @@ object Solution {
     }
 
 
-    var curentTime = 0
+    var currentTime = 0
+    var waitingTimeSum =0
     var finalList = new scala.collection.mutable.ArrayBuffer[order]()
    // println(sortedList)
-    var pQ  = new mutable.PriorityQueue[order]()
-    while(sortedList.nonEmpty || pQ.nonEmpty)
+    var pQ  = new mutable.PriorityQueue[order]( Ordering.by(_.processingTime).reverse).reverse
+    while(customerList.nonEmpty || pQ.nonEmpty)
     {
-      if (sortedList.nonEmpty){
-        sortedList.filter( p=> p.startTime <= curentTime  ).map(pQ.enqueue(_))
-         sortedList= sortedList.filter( p=> p.startTime > curentTime  )
-
+      if (customerList.nonEmpty){
+       // customerList.filter( p=> p.startTime <= currentTime  ).map(pQ.enqueue(_))
+        // sortedList= sortedList.filter( p=> p.startTime > currentTime  )
+        pQ.enqueue(customerList.dequeue())
       }
-
 
 
 
       if (pQ.nonEmpty){
        // println("----")
-        //println(sortedList)
+        println(customerList)
         //println(pQ)
         var currObj=  pQ.dequeue()
         finalList+=currObj
+       println(currentTime)
+        println(waitingTimeSum)
+        waitingTimeSum= waitingTimeSum+ currentTime-currObj.startTime +currObj.processingTime
+
         //sortedList = sortedList.filter(p=> p.startTime != currObj.startTime && p.processingTime  != currObj.processingTime)
-        curentTime=curentTime+ currObj.processingTime
+        currentTime=currentTime+ currObj.processingTime
 
       }
       else{
-        curentTime=curentTime+1
+        currentTime=currentTime+1
       }
 
 
@@ -84,37 +93,39 @@ object Solution {
 
     //print the final Result
    // println(finalList)
-    var waitingTimeSum =0
-    var currentTime=finalList(0).startTime
-    for (item <- finalList){
-      //println(waitingTimeSum)
-      //println(currentTime)
-      waitingTimeSum= waitingTimeSum+ currentTime   -item.startTime +item.processingTime
-      currentTime=currentTime +item.processingTime
+      /*
+       waitingTimeSum =0
+      var currentTime=finalList(0).startTime
+      for (item <- finalList){
+        //println(waitingTimeSum)
+        //println(currentTime)
+        waitingTimeSum= waitingTimeSum+ currentTime   -item.startTime +item.processingTime
+        currentTime=currentTime +item.processingTime
+      }
+      */
+      println((waitingTimeSum.toDouble / finalList.length).toInt)
+     // out.print((waitingTimeSum.toDouble / finalList.length).toInt)
     }
 
-    println((waitingTimeSum.toDouble / finalList.length).toInt)
-   // out.print((waitingTimeSum.toDouble / finalList.length).toInt)
+    def initialise(str:String):Unit={
+      //println(str)
+      val obj = order(str.split("[\\s \\n]")(0).toInt , str.split("[\\s \\n]")(1).toInt)
+      //sortedList +=   obj
+      customerList.enqueue(obj)
+      //sortedList=sortedList.sorted
+
+    }
+
+
+    def main(args: Array[String]) {
+      val stdin = scala.io.StdIn
+
+      val printWriter = new PrintWriter(Console.out)
+
+      Solution.process(  printWriter)
+
+
+    }
   }
-
-  def initialise(str:String):Unit={
-    //println(str)
-    val obj = order(str.split("[\\s \\n]")(0).toInt , str.split("[\\s \\n]")(1).toInt)
-    sortedList +=   obj
-    //sortedList=sortedList.sorted
-
-  }
-
-
-  def main(args: Array[String]) {
-    val stdin = scala.io.StdIn
-
-    val printWriter = new PrintWriter(Console.out)
-
-    Solution.process(  printWriter)
-
-
-  }
-}
 
 
